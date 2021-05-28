c9952db565b1962cf501b7ef04d20e0eded01313
MisterC-Rogers <c.a.r0g3r5@gmail.com>
2021-05-27 21:18:03 -0700

    working on SEO

--- components/SiteHead.js
+++ components/SiteHead.js
@@ -4,12 +4,12 @@ const SiteHead = ({ title, description }) => {
     const image = 'http://www.charlierogers.tech/Charlie-Rogers.png'
     return (
         <Head>
-            <html lang='en' />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content={description} />
+
             <title>{title}</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta property="title" content={title} key="title" />
             <meta charSet="utf-8" />
-            <meta name="description" content={description} />
             {/* Twitter */}
             <meta name="twitter:card" content="summary" key="twcard" />
             <meta name="twitter:creator" content='MisterCRogers' key="twhandle" />
@@ -19,6 +19,7 @@ const SiteHead = ({ title, description }) => {
             <meta property="og:site_name" content="Charlie Rogers" key="ogsitename" />
             <meta property="og:url" content="https://www.charlierogers.tech" key="ogurl" />
             <meta property="og:image" content={image} key="ogimage" />
+            <html lang='en' />
                 
             <link rel="icon" href="/Charlie-Rogers.png" />
         </Head>
